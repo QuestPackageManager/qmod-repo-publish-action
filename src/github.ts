b6ed4ee713cb9f8d71055e183fd27e13da0e2918
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-07-07 20:54:43 -0400

    Initial action setup

---
+++ src/github.ts
@@ -0,0 +1,181 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { GitHub } from '@actions/github/lib/utils'
+import { ModJSON } from './main'
+
+export type GithubRepo = Awaited<
+  ReturnType<InstanceType<typeof GitHub>['rest']['repos']['get']>
+>['data']
+
+export type GithubRepoLite = GithubRepo & NonNullable<GithubRepo['parent']>
+
+export async function getFork(
+  octokit: InstanceType<typeof GitHub>,
+  repoOwner: string,
+  repoName: string
+): Promise<GithubRepoLite> {
+  core.info('Getting Fork of Mod Repo')
+
+  try {
+    const forkedModRepo = await octokit.rest.repos
+      .get({
+        owner: github.context.repo.owner,
+        repo: repoName
+      })
+      .then(x => x.data) as GithubRepoLite
+
+    return forkedModRepo
+  } catch {
+    core.warning('Failed to find a fork of the mod repo, creating it now')
+    core.info('Getting Mod Repo')
+
+    const modRepo = (
+      await octokit.rest.repos.get({
+        owner: repoOwner,
+        repo: repoName
+      })
+    ).data
+
+    core.info('Creating Fork')
+
+    await octokit.rest.repos.createFork({
+      owner: repoOwner,
+      repo: repoName
+    })
+
+    core.info('Getting Fork')
+
+    const forkedModRepo = (
+      await octokit.rest.repos.get({
+        owner: github.context.repo.owner,
+        repo: modRepo.name
+      })
+    ).data
+
+    if (!forkedModRepo.fork) {
+      throw `${forkedModRepo.html_url} is not a fork of https://github.com/${repoName}/${repoOwner}`
+    }
+
+    return forkedModRepo as GithubRepoLite
+  }
+}
+
+export async function CreateBranchIfRequired(
+  octokit: InstanceType<typeof GitHub>,
+  forkedRepo: GithubRepoLite,
+  newBranch: string
+) {
+  core.info(`Checking if "${newBranch}" branch exists`)
+  try {
+    await octokit.rest.git.getRef({
+      owner: forkedRepo.owner.login,
+      repo: forkedRepo.name,
+      ref: `heads/${newBranch}`
+    })
+
+    core.info('Branch already exists')
+  } catch {
+    core.info('Branch does not exists, creating it now')
+
+    const sha = (
+      await octokit.rest.git.getRef({
+        owner: forkedRepo.owner.login,
+        repo: forkedRepo.name,
+        ref: `heads/${forkedRepo.default_branch}`
+      })
+    ).data.object.sha
+
+    await octokit.rest.git.createRef({
+      owner: forkedRepo.owner.login,
+      repo: forkedRepo.name,
+      ref: `refs/heads/${newBranch}`,
+      sha: sha
+    })
+  }
+
+  // This will only run if the branch already existed, as there's a return in the catch statement
+  await FetchUpstream(
+    octokit,
+    forkedRepo,
+    forkedRepo,
+    newBranch,
+    forkedRepo.default_branch
+  )
+}
+
+export async function FetchUpstream(
+  octokit: InstanceType<typeof GitHub>,
+  repo: GithubRepoLite,
+  upstreamRepo: GithubRepoLite,
+  branch: string,
+  upstreamBranch: string
+) {
+  core.info(
+    `Checking if ${repo.owner.login}:${branch} is behind ${upstreamRepo.owner.login}:${upstreamBranch}`
+  )
+
+  const compareResults = (
+    await octokit.rest.repos.compareCommits({
+      owner: upstreamRepo.owner.login,
+      repo: upstreamRepo.name,
+      base: upstreamBranch,
+      head: `${repo.owner.login}:${branch}`
+    })
+  ).data
+
+  if (compareResults.behind_by > 0) {
+    core.info(
+      `${repo.owner.login}:${branch} is behind by ${compareResults.behind_by} commits. Fetching Upstream...`
+    )
+
+    const upstreamBranchReference = (
+      await octokit.rest.git.getRef({
+        owner: upstreamRepo.owner.login,
+        repo: upstreamRepo.name,
+        ref: `heads/${upstreamBranch}`
+      })
+    ).data
+
+    try {
+      await octokit.rest.git.updateRef({
+        owner: repo.owner.login,
+        repo: repo.name,
+        ref: `heads/${branch}`,
+        sha: upstreamBranchReference.object.sha,
+        force: true
+      })
+    } catch (error) {
+      throw `Failed to fetch upstream. This can be fixed by performing a manual merge\nError: ${error}`
+    }
+  } else {
+    core.info(`${repo.owner.login}:${branch} is up-to-date`)
+  }
+}
+
+export async function ConstructModEntry(
+  octokit: InstanceType<typeof GitHub>,
+  modJson: ModJSON,
+  downloadUrl: string
+) {
+  const currentUser = (
+    await octokit.rest.users.getByUsername({
+      username: github.context.repo.owner
+    })
+  ).data
+
+  const authorIcon = currentUser.avatar_url
+
+  const modEntry = {
+    name: modJson.name,
+    description: modJson.description,
+    id: modJson.id,
+    version: modJson.version,
+    downloadLink: downloadUrl,
+    source: `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/`,
+    authorIcon: authorIcon,
+    author: modJson.author,
+    cover: modJson.coverImage
+  }
+
+  return modEntry
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as github from '@actions/github'
+import JSZip from 'jszip'
+import {
+  ConstructModEntry,
+  CreateBranchIfRequired,
+  FetchUpstream,
+  getFork,
+  GithubRepoLite
+} from './github'
+import path from 'path'
+
+export interface ModJSON {
+  name: string
+  id: string
+  description?: string
+
+  version: string
+  modloader: string
+
+  author: string
+
+  coverImage?: string
+  packageId?: string
+  packageVersion?: string
+}
 
 /**
  * The main function for the action.
@@ -7,18 +31,78 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const myToken = core.getInput('token')
+    const qmodUrl = core.getInput('qmod_url')
+    const qmodRepoOwner = core.getInput('qmod_repo_owner')
+    const qmodRepoName = core.getInput('qmod_repo_name')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // You can also pass in additional options as a second parameter to getOctokit
+    // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+    const octokit = github.getOctokit(myToken)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const qmod = await fetch(qmodUrl)
+    const qmodZip = await JSZip.loadAsync(await qmod.blob())
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const modJsonFile = qmodZip.file('mod.json')
+
+    if (modJsonFile == null) {
+      core.error(`mod.json not found in zip ${qmodUrl}`)
+      return
+    }
+
+    const modJson: ModJSON = JSON.parse(await modJsonFile.async('text'))
+    const forkedModRepo = await getFork(octokit, qmodRepoOwner, qmodRepoName)
+    const modRepo = forkedModRepo.parent! as GithubRepoLite
+
+    const newBranch = `${modJson.id}-${modJson.version}-${modJson.packageVersion}`
+
+  
+    await FetchUpstream(
+      octokit,
+      forkedModRepo,
+      modRepo,
+      forkedModRepo.default_branch,
+      modRepo.default_branch
+    )
+    await CreateBranchIfRequired(octokit, forkedModRepo, newBranch)
+
+    // core.info('Cloning fork')
+    // const result = await exec.exec(`git clone ${forkedModRepo.html_url}`)
+    // if (result != 0) {
+    //   throw `Git clone returned error ${result}`
+    // }
+
+    core.info('Encoding modified Mods json')
+
+    const modManifest = ConstructModEntry(octokit, modJson, qmodUrl)
+    // convert to base64
+    const encodedModManifest = btoa(JSON.stringify(modManifest, null, 2))
+
+    core.info('Commiting modified Mods json')
+    await octokit.rest.repos.createOrUpdateFileContents({
+      owner: forkedModRepo.owner.login,
+      repo: forkedModRepo.name,
+      path: path.join('/', 'mods', modJson.packageVersion ?? 'global'),
+      message: `Added ${modJson.name} v${modJson.version} to the Mod Repo`,
+      content: encodedModManifest,
+      branch: `refs/heads/${newBranch}`
+    })
+
+    // make PR
+    const { data: pullRequest } = await octokit.rest.pulls.create({
+      owner: modRepo.owner.login,
+      repo: modRepo.name,
+      
+      title: `${modJson.id} ${modJson.version} - ${modJson.packageId} ${modJson.packageVersion}`,
+      body: 'Automatically generated pull request',
+      
+      base: forkedModRepo.default_branch,
+      head: `${forkedModRepo.owner.login}:${newBranch}`,
+      maintainer_can_modify: true,
+    })
+
+
+    core.info(`Made PR at ${pullRequest.html_url}`)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
